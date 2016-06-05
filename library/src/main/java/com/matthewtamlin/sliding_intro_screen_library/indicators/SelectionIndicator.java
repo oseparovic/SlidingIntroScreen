b3e06c9081f7cddbb42702da9d89b919d0cee2b3
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-06-05 11:25:16 +0930

    Updated SelectionIndicator.java
    Minor Javadoc updates

--- library/src/main/java/com/matthewtamlin/sliding_intro_screen_library/indicators/SelectionIndicator.java
+++ library/src/main/java/com/matthewtamlin/sliding_intro_screen_library/indicators/SelectionIndicator.java
@@ -22,7 +22,7 @@ package com.matthewtamlin.sliding_intro_screen_library.indicators;
 public interface SelectionIndicator {
 	/**
 	 * Sets the index of the currently selected item and updates the UI to reflect the change. The
-	 * {@code index} parameter must not be less than zero, and it must not be equal to or greater
+	 * {@code index} parameter must not be less than 0, and it must not be equal to or greater
 	 * than the current size of the indicator.
 	 *
 	 * @param index
