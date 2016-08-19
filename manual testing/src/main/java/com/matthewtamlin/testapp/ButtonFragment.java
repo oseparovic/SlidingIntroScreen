2bfe64a05ccbfc92e00f925bb9d534bd6fe5e4db
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-08-19 18:05:42 +0930

    Updated ButtonFragment.java
    Refactored class

--- manual testing/src/main/java/com/matthewtamlin/testapp/ButtonFragment.java
+++ manual testing/src/main/java/com/matthewtamlin/testapp/ButtonFragment.java
@@ -23,7 +23,16 @@ public class ButtonFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
 			@Nullable Bundle savedInstanceState) {
-		// Create the button and set its layout params
+		final Button button = createButton();
+		final LinearLayout layout = createLayout();
+		layout.addView(button);
+		return layout;
+	}
+
+	/**
+	 * @return a Button which is centred in its parent and provides visual feedback when clicked
+	 */
+	private Button createButton() {
 		final Button button = new Button(getContext());
 		button.setText("Button inside fragment");
 		button.setOnClickListener(new View.OnClickListener() {
@@ -32,17 +41,22 @@ public class ButtonFragment extends Fragment {
 				button.setText("Button click successful...");
 			}
 		});
-		final LinearLayout.LayoutParams buttonLayoutParams = new LinearLayout.LayoutParams(ViewGroup
-				.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-		buttonLayoutParams.gravity = Gravity.CENTER;
-		button.setLayoutParams(buttonLayoutParams);
 
-		// Create the root view and set its layout params
-		final LinearLayout layout = new LinearLayout(getContext());
-		layout.addView(button);
 		final LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup
-				.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-		layout.setLayoutParams(layoutParams);
+				.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+		layoutParams.gravity = Gravity.CENTER;
+		button.setLayoutParams(layoutParams);
+
+		return button;
+	}
+
+	/**
+	 * @return a LinearLayout which fills its parent
+	 */
+	private LinearLayout createLayout() {
+		final LinearLayout layout = new LinearLayout(getContext());
+		layout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+				ViewGroup.LayoutParams.MATCH_PARENT));
 
 		return layout;
 	}
