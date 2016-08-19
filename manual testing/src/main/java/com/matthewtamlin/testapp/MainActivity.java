60990012a04d7baeeb1849e94e086762b434b42d
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-08-19 17:52:48 +0930

    Renamed SelectorActivity to MainActivity
    - Updated class
    - Updated references

--- manual testing/src/main/java/com/matthewtamlin/testapp/MainActivity.java
+++ manual testing/src/main/java/com/matthewtamlin/testapp/MainActivity.java
@@ -21,7 +21,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class SelectorActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
