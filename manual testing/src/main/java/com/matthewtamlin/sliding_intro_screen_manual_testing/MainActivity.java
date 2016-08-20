6129b1a8132c61ef6b54dbe630cb38ff170bd84c
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-08-21 08:50:26 +0930

    Updated MainActivity.java
    Added Javadoc

--- manual testing/src/main/java/com/matthewtamlin/sliding_intro_screen_manual_testing/MainActivity.java
+++ manual testing/src/main/java/com/matthewtamlin/sliding_intro_screen_manual_testing/MainActivity.java
@@ -21,6 +21,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+/**
+ * Activity for selecting the manual test to run.
+ */
 public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,42 +31,89 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 	}
 
-
+	/**
+	 * Launches the button config test.
+	 *
+	 * @param v
+	 * 		the View which was clicked to launch the test
+	 */
 	public void testButtonConfig(View v) {
 		Intent i = new Intent(this, TestButtonConfig.class);
 		startActivity(i);
 	}
 
+	/**
+	 * Launches the multiple page behaviour test.
+	 *
+	 * @param v
+	 * 		the View which was clicked to launch the test
+	 */
 	public void testMultiplePageBehaviour(View v) {
 		Intent i = new Intent(this, TestMultiplePageBehaviour.class);
 		startActivity(i);
 	}
 
+	/**
+	 * Launches the single page behaviour test.
+	 *
+	 * @param v
+	 * 		the View which was clicked to launch the test
+	 */
 	public void testSinglePageBehaviour(View v) {
 		Intent i = new Intent(this, TestSinglePageBehaviour.class);
 		startActivity(i);
 	}
 
+	/**
+	 * Launches the selection indicator config test.
+	 *
+	 * @param v
+	 * 		the View which was clicked to launch the test
+	 */
 	public void testSelectionIndicatorConfig(View v) {
 		Intent i = new Intent(this, TestSelectionIndicatorConfig.class);
 		startActivity(i);
 	}
 
+	/**
+	 * Launches the page lock test.
+	 *
+	 * @param v
+	 * 		the View which was clicked to launch the test
+	 */
 	public void testPageLock(View v) {
 		Intent i = new Intent(this, TestPageLock.class);
 		startActivity(i);
 	}
 
+	/**
+	 * Launches the transformer test.
+	 *
+	 * @param v
+	 * 		the View which was clicked to launch the test
+	 */
 	public void testTransformer(View v) {
 		Intent i = new Intent(this, TestTransformer.class);
 		startActivity(i);
 	}
 
+	/**
+	 * Launches the hide status bar test.
+	 *
+	 * @param v
+	 * 		the View which was clicked to launch the test
+	 */
 	public void testHideStatusBar(View v) {
 		Intent i = new Intent(this, TestHideStatusBar.class);
 		startActivity(i);
 	}
 
+	/**
+	 * Launches the behaviours test.
+	 *
+	 * @param v
+	 * 		the View which was clicked to launch the test
+	 */
 	public void testBehaviours(View v) {
 		Intent i = new Intent(this, TestBehaviours.class);
 		startActivity(i);
